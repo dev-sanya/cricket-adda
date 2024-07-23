98b20533c71247c84b2f4a61291f0538f8270609
dev-sanya <157735624+dev-sanya@users.noreply.github.com>
2024-07-23 17:22:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+/*const url = 'https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '75d654b57amshcaef0cd023f388ep133c0fjsn9ff32ffa30cd',
+		'x-rapidapi-host': 'cricbuzz-cricket.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    const apiKey = 'c99bf3b5fcmsh78bf7999ec5b408p19fb4bjsn76999f05c7d5'; 
+    const apiUrl = 'https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent'; 
+    async function fetchScores() {
+        try {
+            const response = await fetch(apiUrl, {
+                headers: {
+                    'x-rapidapi-key': '75d654b57amshcaef0cd023f388ep133c0fjsn9ff32ffa30cd',
+		            'x-rapidapi-host': 'cricbuzz-cricket.p.rapidapi.com'
+	
+                }
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            const data = await response.json();
+            displayScores(data);
+        } catch (error) {
+            console.error('Error fetching scores:', error);
+        }
+    }
+    function displayScores(data){
+        console.log(data);
+
+    }
+    fetchScores();
+});
